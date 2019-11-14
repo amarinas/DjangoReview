590c116aba1de7285a073118f3ce4f26757a4d9d
ali <ali.marinas@icloud.com>
2019-11-14 01:26:54 -0800

    added testcase for blog site tdd

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from .models import Post
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+class PostModelTest(TestCase):
+
+    def test_string_representation(self):
+        post = Post(title="My Post title")
+        self.assertEqual(str(post), post.title)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Post._meta.verbose_name_plural), "posts")
+
+    class Meta:
+        verbose_name_plural = "posts"
+
+class ProjectTest(TestCase):
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+class HomePageTests(TestCase):
+
+    """Test whether our blog entries show up on the homepage"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create(username='some_user')
+
+    def test_one_entry(self):
+        Post.objects.create(title='title', text='text', author=self.user)
+        response = self.client.get('/')
+        self.assertContains(response, 'title')
+        self.assertContains(response, 'text')
+
+    def test_two_entries(self):
+        Post.objects.create(title='title', text='text', author=self.user)
+        Post.objects.create(title='title', text='text', author=self.user)
+        response = self.client.get('/')
+        self.assertContains(response, 'title')
+        self.assertContains(response, 'text')
+        self.assertContains(response, 'title')
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.views.generic import ListView
+from blog.models import Post
 
 
 def post_list(request):
@@ -38,3 +40,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form':form})
+
+
+class HomeView(ListView):
+    template_name = 'base.html'
+    queryset = Post.objects.order_by('-published_date')
